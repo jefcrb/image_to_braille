f872a1fe31d3d91fa48596bcc2adac32c494547c
jefcrb <83902653+jefcrb@users.noreply.github.com>
2022-03-29 16:29:14 +0200

    Add files via upload

--- image_to_braille.py
+++ image_to_braille.py
@@ -23,14 +23,13 @@ def help():
     print("Usage: python image_to_braille.py <image>")
     exit()
 
-
 image = check_img(sys.argv)
 
 braille_characters = ""
 for i in range(0,256):
     braille_characters += chr(0x2800 + i)
 
-image = image.quantize(2)
+image = image.convert("L")
 
 height = int(image.size[1]/4)
 width = int(image.size[0]/2)
@@ -48,24 +47,27 @@ for r in range(height):
                 except:
                     print("error")
         val = 0
-        if char[0] == 1:
+        if char[0] < 127:
             val += 1
-        if char[1] == 1:
+        if char[1] < 127:
             val += 8
-        if char[2] == 1:
+        if char[2] < 127:
             val += 2
-        if char[3] == 1:
+        if char[3] < 127:
             val += 16
-        if char[4] == 1:
+        if char[4] < 127:
             val += 4
-        if char[5] == 1:
+        if char[5] < 127:
             val += 32
-        if char[6] == 1:
+        if char[6] < 127:
             val += 64
-        if char[7] == 1:
+        if char[7] < 127:
             val += 128
         try:
-            braille_img += braille_characters[val]
+            if val != 0:
+                braille_img += braille_characters[val]
+            else:
+                braille_img += " "
         except:
             print(val)
     braille_img += "\n"
